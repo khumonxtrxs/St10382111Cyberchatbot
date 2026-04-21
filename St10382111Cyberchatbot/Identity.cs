f0ec480b8932c18266b83438d66a407ceaa48026
KHUMO TWALA ST10382111 <166536070+khumonxtrxs@users.noreply.github.com>
2026-04-21 22:56:43 +0200

    Created Identity.cs to handle user name input and validation

---
+++ St10382111Cyberchatbot/Identity.cs
@@ -0,0 +1,21 @@
+﻿
+using St10382111Cyberchatbot.Utilities;
+using System;
+
+namespace St10382111Cyberchatbot
+{
+   
+        public static class Identity
+        {
+            public static void NamePrompt()
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("WHAT IS YOUR NAME?");
+                string name = Console.ReadLine();
+
+                Console.ResetColor();
+                AsciiHelper.SyncSpeakAndType($"Nice to meet you {name}. Let's chat.");
+                Cyrex.Start(name);
+            }
+        }
+    }
