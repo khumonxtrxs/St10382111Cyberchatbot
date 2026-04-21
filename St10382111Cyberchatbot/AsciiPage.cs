00a2b08771dd5b11e24d091663329149d720f71b
KHUMO TWALA ST10382111 <166536070+khumonxtrxs@users.noreply.github.com>
2026-04-21 22:55:10 +0200

    Created AsciiPage class for chatbot welcome screen

---
+++ St10382111Cyberchatbot/AsciiPage.cs
@@ -0,0 +1,32 @@
+﻿using St10382111Cyberchatbot.Utilities;
+using System;
+
+namespace St10382111Cyberchatbot
+{
+  
+
+        public static class AsciiPage
+        {
+            public static void Show()
+            {
+                HelperSpeech.PlayGreeting();
+
+                Console.Clear();
+                Console.Title = "Cyrex Chat";
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                string asciiart = @"  _______       _______  ________   __   _____ _   _       _______ ____   ____ _______ 
+ / ____\ \     / / __ \|  ____\ \ / /  / ____| |  | |   /\__   __| _ \ / __ \__   __|
+| |     \ \_/ /| |__) | |__   \ V /  | |    | |__| |  /  \  | |  | |_) | |  | | | |   
+| |      \   / |  _  /|  __|   > <   | |    |  __  | / /\ \ | |  |  _ <| |  | | | |   
+| |____   | |  | | \ \| |____ / . \  | |____| |  | |/ ____ \| |  | |_) | |__| | | |   
+ \_____|  |_|  |_|  \_\______/_/ \_\  \_____|_|  |_/_/    \_\_|  |____/ \____/  |_|   ";
+
+                AsciiHelper.DrawBorderBox(asciiart);
+                Console.WriteLine();
+                AsciiHelper.SyncSpeakAndType("Hello! I am Cyrex. Making Cybersecurity better for you.");
+
+                Console.ResetColor();
+            }
+        }
+    }
