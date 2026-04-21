9bc400f2cc87c6e9d39edc4d2b1bd31d30f3ba69
KHUMO TWALA ST10382111 <166536070+khumonxtrxs@users.noreply.github.com>
2026-04-21 23:33:18 +0200

    updated GetResponses

--- St10382111Cyberchatbot/Cyrex.cs
+++ St10382111Cyberchatbot/Cyrex.cs
@@ -21,10 +21,18 @@ namespace St10382111Cyberchatbot
                 }
             }
 
-            public static string GetResponse(string input)
-            {
-                if (input.Contains("password")) return "Use strong passwords.";
-                return "I'm listening.";
-            }
+        public static string GetResponse(string input)
+        {
+            string clean = input?.ToLower() ?? "";
+
+            if (clean.Contains("how are you")) return "I'm just a code, haha haha but i am funtioning well thank you.";
+            if (clean.Contains("purpose")) return "My purpose is to answer your cybersecurity questions.";
+            if (clean.Contains("what can i ask")) return "You can ask me about passwords, phishing and safe browsing.";
+            if (clean.Contains("password")) return "Use strong, unique passwords.";
+            if (clean.Contains("phishing")) return "Never click links from unknown emails.";
+            if (clean.Contains("safe browsing")) return "Make use of HTTPS, and enamble two (2) factor authentication and avoid links that asr suspicious.";
+
+            return "I'm not sure about that. Can you rephrase?";
         }
     }
+}   
